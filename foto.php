902e5e0bfadcc90218355cb8f2b3c10d770fd107
fernando <mago_100_10@hotmail.com>
2014-09-05 08:29:25 -0500

    subiendo

--- foto.php
+++ foto.php
@@ -13,31 +13,36 @@
 		?>
     	
         
-<<<<<<< HEAD
-        	<div class="conmiv">
-            <div class="contpa">
-            	<span class="pa1">Paquete 1</span>
-                <span class="tc1"> - Tiempo de Cobertura 1:30 hrs. de foto sesión   Incluye:</span>
-<span class=""-1 CD personalizado con 150 fotos seleccionadas en alta resolución -Retoque básico en 75 de las fotos de su selección 
--1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show) 
--1 Álbum impreso 8x6" con 30 de las fotos retocadas, en alta resolución 
--4 fotos sorpresa en alta resolución con retoque artístico 
-COSTO $750 USD
-            	</div>
-=======
+
+        
+       
+
         <div class="almiv">
         
         	<div class="paquet">
                 <a href="foto.php" class="pb"> <span class="pbs">paquete basico</span></a>
                 <a href="foto-pe.php" class="pe">paquete especial</a>
                 <a href="foto-mr.php" class="mr">mesa de regalo</a>
->>>>>>> 652895d320d3fd27c559f7acb8e66966169bd9c5
+
             </div>
         
             <div class="miv">
             
                 <div class="conmiv">
-    
+                <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p> Paquete 1</p>
+                - Tiempo de Cobertura 1:30 hrs. de foto sesión  
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 150 fotos seleccionadas en alta resolución.
+				<br>-Retoque básico en 75 de las fotos de su selección.
+				<br>-1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 8x6" con 30 de las fotos retocadas, en alta resolución.
+				<br>-4 fotos sorpresa en alta resolución con retoque artístico.</div>
+<span class="tc2">$750.00 USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
                 </div>
             
             </div>
