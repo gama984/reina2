880f1f05e8f015aa93c5d175cd33c9a7d45aeead
fernando <mago_100_10@hotmail.com>
2014-09-06 08:33:11 -0500

    subeindo

--- foto-pe.php
+++ foto-pe.php
@@ -23,8 +23,78 @@
         
             <div class="miv">
             
-                <div class="conmiv">
+                <div class="conmiv pq12">
+                                <div class="tc4">
+                <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p> Engagement</p>
+                -Si tienes pensada una cena romántica con tu pareja a la luz de las velas y aprovechar la ocasión para sorprenderla con la pregunta mas importante de sus vidas? si quieres que el recuerdo del momento quede guardado para siempre esta es tu mejor oportunidad.
+                <p><br>- Tiempo de Cobertura 1hrs. de foto sesión </p>
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 100 fotos en alta resolución.
+				<br>-1DVD personalizado y musicalizado con las fotos de la sesión en alta resolución (slide show).
+				<br>-10 fotos sorpresa en alta resolución con retoque artístico.
+				<span class="tc5">$450.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+  
+   
+    </div>
     
+    
+        <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Sexy Masquerade</p>
+               -Te gustaría sorprender a tu pareja y mostrarle tu lado mas sexy y travieso? atrevete a hacerlo con esta foto sesión en tu mejor lencería y dejanos añadirle ademas un toque de fantasía llevando puesta una mascara al mas elegante estilo de las mejores fiestas de mascaras europeas.(El paquete no incluye lencería ni maquillaje).
+                <p><br>- Tiempo de Cobertura 1hrs. de foto sesión </p>
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 100 fotos en alta resolución.
+				<br>-1DVD personalizado y musicalizado con las fotos de la sesión en alta resolución (slide show).
+				<br> -10 fotos sorpresa en alta resolución con retoque artístico.
+                
+				<span class="tc5">$400.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+
+   
+    </div>
+    <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Trash the dress</p>
+               -Tu y tu pareja son de los que les gusta divertirse nadando en la piscina o en el mar sin importar que llevas puesto?, aun si tu único traje de baño del momento es el mismo atuendo de tu ceremonia de bodas?, entonces este paquete es perfecto para ti.
+                <p><br>- Tiempo de Cobertura 1hrs. de foto sesión </p>
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 100 fotos en alta resolución.
+				<br> -1DVD personalizado y musicalizado con las fotos de la sesión en alta resolución (slide show).
+				<br>  -10 fotos sorpresa en alta resolución con retoque artístico.
+                
+				<span class="tc5">$650.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+
+   
+    </div>
+       
+<div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Savage Pool</p>
+               -Te gustaría vivir una experiencia única y capturar el momento para siempre entrando con tu pareja en uno de los lugares milenarios mas enigmáticos y hermosos del planeta? ven y prueba nuestra versión de trash the dress en un cenote.(no incluye costo del transporte al lugar del cenote).
+                <p><br>- Tiempo de Cobertura 1hrs. de foto sesión </p>
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 100 fotos  en alta resolución.
+				<br>-1DVD personalizado y musicalizado con las fotos de la sesión en alta resolución (slide show).
+				<br>-10 fotos sorpresa en alta resolución con retoque artístico.
+                
+				<span class="tc5">$950.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+
+   
+    </div>
                 </div>
             
             </div>
--- foto.php
+++ foto.php
@@ -28,7 +28,8 @@
         
             <div class="miv">
             
-                <div class="conmiv">
+                <div class="conmiv pq12">
+                <div class="tc4">
                 <div class="tc3">
                      <div class="contpa">
             	<div class="tc1"><p> Paquete 1</p>
@@ -43,6 +44,98 @@
             	</div>
                 <div class="trian"></div>
     </div>
+    
+    <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p> Paquete 2</p>
+                - Tiempo de Cobertura 2 hrs. de foto sesión 
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 200 fotos seleccionadas en alta resolución
+				<br> -Retoque básico en 100 de las fotos de su selección
+				<br>-1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 8 x6" con 40 de las fotos retocadas en alta resolución.
+				<br>-6 fotos sorpresa en alta resolución con retoque artístico.</div>
+<span class="tc2">$850.00 USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+    
+        <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p> Paquete 3</p>
+               - Tiempo de Cobertura 2:30 hrs. de foto sesión.  
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 250 fotos seleccionadas en alta resolución.
+				<br> -Retoque básico en 125 de las fotos de su selección.
+				<br>-1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 12x9" con 50 de las fotos retocadas.
+				<br>-8 fotos sorpresa en alta resolución con retoque artístico.</div>
+<span class="tc2">$1150.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+            <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Paquete 4</p>
+               - Tiempo de Cobertura 3 hrs. de foto sesión 
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 300 fotos seleccionadas en alta resolución.
+				<br> -Retoque profundo en 150 de las fotos de su selección.
+				<br>-1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 12x9" con 60 de las fotos retocadas.
+				<br>-10 fotos sorpresa  en alta resolución con retoque artístico.</div>
+<span class="tc2">$1250.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+    <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Paquete 5</p>
+                - Tiempo de Cobertura 3:30 hrs. de foto sesión 
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 350 fotos seleccionadas en alta resolución.
+				<br> -Retoque profundo en 175 de las fotos de su selección.
+				<br>-1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 17x12" con 70 de las fotos retocadas.
+				<br> -12 fotos sorpresa en alta resolución con retoque artístico.</div>
+<span class="tc2">$1550.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+       
+    <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Paquete 5</p>
+                - Tiempo de Cobertura 3:30 hrs. de foto sesión 
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 350 fotos seleccionadas en alta resolución.
+				<br> -Retoque profundo en 175 de las fotos de su selección.
+				<br>-1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 17x12" con 70 de las fotos retocadas.
+				<br> -12 fotos sorpresa en alta resolución con retoque artístico.</div>
+<span class="tc2">$1550.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+    
+    <div class="tc3">
+                     <div class="contpa">
+            	<div class="tc1"><p>Paquete 6</p>
+                - Tiempo de Cobertura 4 hrs. de foto sesión 
+                <p><br>Incluye:</p>
+				 	-1 CD personalizado con 400 fotos seleccionadas en alta resolución.
+				<br>  -Retoque profundo en 200 de las fotos de su selección
+				<br> -1DVD personalizado y musicalizado de las fotos retocadas en alta resolución (slide show).
+				<br>-1 Álbum impreso 17x12" con 80 de las fotos retocadas.
+				<br>-14 fotos sorpresa en alta resolución con retoque artístic.</div>
+<span class="tc2">$1650.00USD</span>
+            	</div>
+                <div class="trian"></div>
+    </div>
+        
+  
+    </div>
+     
                 </div>
             
             </div>
