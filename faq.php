cd162b16bf62a7fb897cd35d97ef26022c9361f0
fernando <mago_100_10@hotmail.com>
2014-09-15 03:43:06 -0500

    subiendo

--- faq.php
+++ faq.php
@@ -20,10 +20,10 @@
             
             
                 <div class="conmiv pq12">
-<<<<<<< HEAD
                 <div class="tc4">
                 <div class="tc-3">
                      <div class="contpa">
+                     <?php $faq[0] ?>
           Preguntas frecuentes
 <p class="p1"><br><a class="texto" href="javascript:MostrarOcultar('texto1');"> ¿Cómo puedo contratar tus servicios para el día de mi boda y cuál es el procedimiento?</a></p>
 
@@ -99,91 +99,8 @@
                 
     </div>
     
-=======
-                    <div class="tc4">
-                        <div class="tc-3">
-                             <div class="contpa">
-                                <?php $faq[0] ?>
-                                
-                                <!-- Inicia -->
-                                
-                                	 Preguntas frecuentes
-<p class="p1"><br>¿Cómo puedo contratar tus servicios para el día de mi boda y cuál es el procedimiento?</p>
 
-<p class="p2"><br>Puedes contactar con nosotros directamente por teléfono o bien vía e-mail para agendar tu sesión o si lo deseas también puedes consultar con nosotros a través de nuestro sitio web en la “sección de contacto”, donde con gusto nuestro personal te atenderá. Se te pedirá llenar una hoja de formulario con los datos de tu sesión, la fecha, hora y el lugar así como tus datos de contacto, el tipo de servicio requerido y forma de pago de tu preferencia, una vez llenado tu formulario deberás hacer el depósito en efectivo o con tarjeta de crédito vía bancaria o paypal por la cantidad total correspondiente a tu paquete y enviarnos tu comprobante de pago. Una vez se haya confirmado el depósito, la sesión quedará registrada en nuestro calendario.</p>
-
-<p class="p1"> <br>¿En qué lugares ofreces tus servicios?</p>
-
-<p class="p2"><br>Por el momento nuestros servicios únicamente están disponibles para la zona del Caribe Mexicano, dentro de la Riviera Maya, en caso de que tu sesión este programada en alguna zona fuera de nuestra ciudad, un costo extra por gastos de traslado del fotógrafo será aplicado al precio de tu paquete.</p>
-
-<p class="p1"><br>¿Qué tipo de equipo fotográfico utilizan para sus sesiones?</p>
-
-<p class="p2"><br>Nosotros utilizamos una gran variedad de equipos y modelos tanto de cámaras como de lentes especiales para cada proyecto que realizamos, cada uno de nuestros fotógrafos cuenta con su propio equipo profesional, si deseas mayor información al respecto solo tienes que revisar las fichas técnicas de nuestros fotógrafos en la “sección de portafolios”, donde podrás también apreciar algunos ejemplos de su trabajos más recientes.  </p>
-<p class="p1"><br>¿Cuál es el costo de tus sesiones?</p>
-
-<p class="p2"><br>Nuestro costo por sesión de fotografía sencilla con 1 fotógrafo y sin inclusiones, es de $330 usd. Por hora de sesión, pero además contamos con diversos paquetes de fotografía que incluyen otros beneficios, te recomendamos consultar en nuestro sitio Web en la “sección de servicios” para conocer todos los detalles de cada paquete, seguramente tenemos algo que se adapte a tus necesidades.</p>
-
-<p class="p1"><br>¿Con cuánto tiempo de anticipación necesito apartar mi sesión?</p>
-
-<p class="p2"><br>Para asegurarte de que tengamos completa disponibilidad para la fecha de tu sesión, te recomendamos reservarla por lo menos de 2 a 6 meses previos a la fecha del evento y realizar el depósito por la cantidad total de tu paquete ya que solo así tu sesión quedara agendada. </p>
-
-<p class="p1"><br>¿Qué tengo que hacer en caso de requerir que un fotógrafo en particular haga mi sesión?</p>
-
-<p class="p2"><br>Para registrar los servicios de un fotógrafo en particular consulta su agenda dando clic en el icono del calendario que aparece en la ventana de su ficha técnica, en la sección de portafolios de nuestra página. En caso de estar disponible solo debes dar clic en el botón “apartar fotógrafo”, que se activará automática mente debajo de tu hoja de registro 15 días antes de la fecha de tu evento por un periodo de 72 horas, en caso de no activar esta parte del servicio durante este periodo, nosotros asignaremos a uno de nuestros fotógrafos por ti.  </p>
-
-<p class="p1"><br>¿Por qué tengo que pagar la comida del fotógrafo / videógrafo?</p>
-
-<p class="p2"><br>Cuando contratas nuestros servicios todo nuestro tiempo y esfuerzo está enfocado a satisfacer tus necesidades, incluso muchas veces dejando de lado las nuestras, para así asegurarnos de no perder ningún detalle y no creemos que ofrecer un platillo de comida para tu fotógrafo / videógrafo dentro de tu hotel o lugar de la sesión sea una obligación, más bien lo vemos como un buen gesto de cortesía de tu parte, en especial en el caso de sesiones largas de  6 o más horas donde un poco de caviar o langosta con una copa de vino tinto no le caen nada mal a nadie...  Aun que...  Pensándolo bien incluso un rico Club Sándwich y una soda son bien recibidos, tú qué dices obligación o cortesía.</p>
-
-<p class="p1"><br>¿Qué sigue ahora que ya termino mi sesión de fotos?</p>
-
-<p class="p2"><br>Una vez finalizada tu sesión, el fotógrafo te notificara que su trabajo ha concluido y te solicitara amablemente nos proporciones un correo electrónico vigente y un teléfono personal para ponernos en contacto contigo. En un plazo no mayor a 48 horas posteriores al evento recibirás vía e-mail un archivo Excel y un link a una galería virtual en baja resolución, donde podrás revisar todas las fotos de tu sesión, desde ahí, debes elegir y anotar en tu archivo el nombre o numero de las fotos que deseas para tu paquete y reenviarnos esa lista para que podamos continuar con la siguiente fase del proceso.</p>
-
-<p class="p1"><br>¿Qué plazo tengo para reenviarte el archivo Excel con la selección de mis fotos?</p>
-
-<p class="p2"><br>A partir de que te enviemos tu  link tendrás un plazo límite de 4 semanas,  tiempo que consideramos más que suficiente para revisar tu galería virtual con calma desde casa, elegir tus fotos y reenviarnos el archivo Excel con la lista de tú selección, una vez finalizado este periodo, la galería virtual se bloqueara y eliminara de nuestras bases de datos y tu link quedara inactivo, esto lo hacemos por cuestiones de limpieza, orden y seguridad de nuestro servidor, por lo que en caso de no haber podido concluir tu selección a tiempo, tendrás que solicitarnos enviarte un link nuevo.</p>
-<p class="p1"><br>¿Qué paso sigue una vez que envíe el archivo de mi selección?</p>
-
-<p class="p2"><br>Cada archivo que nosotros recibimos sigue un mismo proceso y en el momento de recepción de cada lista, la revisamos para verificar que todo este correcto. En caso de no encontrar ningún problema, te enviaremos a tu correo electrónico un numero de orden, el cual se asigna de forma consecutiva a cada archivo que recibimos correctamente, de acuerdo a la fecha y hora de su recepción. Una vez asignado este numero de orden no puede ser brincado, omitido o modificado por ningún motivo, lo que nos permite tener un mayor control y seguir un proceso más lineal y justo para cada cliente.</p>
-<p class="p1"><br>¿Cómo puedo verificar cual es el status de mi paquete?</p>
-
-<p class="p2"><br>El numero de orden que nosotros te enviamos te servirá como referencia para poder conocer en todo momento cual es el status de tu paquete durante el resto del proceso, esto lo puedes revisar entrando directamente desde el “panel de control” de nuestro sitio Web con tu cuenta de usuario.</p>
-<p class="p1"><br>¿Cuánto tiempo debo esperar para recibir mi paquete completo?</p>
-
-<p class="p2"><br>Los tiempos de entrega para un paquete recibido correctamente en tiempo y orden van de 4 a 8 semanas dependiendo del paquete y del nivel de retoque contratados.  El conteo regresivo para la entrega de cada paquete comienza a partir del momento en que se inicia el retoque de las fotografías. Recuerda que puedes verificar en todo momento el status de tu paquete simplemente consultando con tu numero de orden en la “sección del panel de control” de nuestra página. </p>
-
-<p class="p1"><br>¿El costo de mi paquete incluye el envío?</p>
-
-<p class="p2"><br>Sí, todos nuestros paquetes ya incluyen el costo de envío, para esto contamos con el servicio de 2 empresas de paquetería con servicio de entrega a cualquier parte del mundo, DHL y UPS de las cuales tu puedes elegir si así lo deseas, la que sea de tu preferencia.</p>
-
-<p class="p1"><br>¿qué pasa si quiero mis fotos impresas?</p>
-
-<p class="p2"><br>Por políticas de la empresa y nuestro compromiso con el cuidado del medio ambiente, ninguno de nuestros paquetes incluye directamente el servicio de impresión fotográfica, salvo en el caso del álbum, que se imprime de forma muy distinta al proceso utilizado para el revelado de fotografía, en el cual se requieren de químicos tóxicos y dañinos al medio ambiente.  Sin embargo, si aun así tú deseas adquirir tus fotos impresas por medio de nosotros, puedes solicitárnoslo vía correo electrónico, ten en cuenta que este servicio es externo, lo que implica pagar un cargo extra al precio original de tu paquete, que variara de acuerdo a la cantidad de fotos, el tamaño y el tipo de acabado del papel solicitado. Para mayor información consulta nuestras listas de precios en la “sección de servicios” en nuestra página.</p>
-
-<p class="p1"><br>¿Qué pasa si envíe mi archivo de selección, pero me informaron que hay un error en mi lista?</p>
-
-<p class="p2"><br>Cuando nosotros detectamos un error en alguna lista de selección de un cliente como por ejemplo, varios nombres de fotos repetidos o que la cantidad de fotos especificada no coincide con la correspondiente al paquete contratado, ya sea porque contiene fotos de más o por que le falten fotos, te notificaremos por correo electrónico. En este punto tu paquete no recibirá un numero de orden y nos veremos obligados a detener temporalmente la siguiente fase del proceso, para que así puedas corregir tu lista y devolvérnosla a la mayor brevedad posible, solo hasta que hayamos recibido nuevamente tu lista corregida podrá pasar a la siguiente etapa del proceso, lo que resultara en un retraso en tus tiempos de entrega, los cuales pueden aumentar considerablemente de acuerdo a la cantidad de carga de trabajo del momento.  Por lo que te recomendamos tomar muy en cuenta este punto y revisar minuciosamente tu lista antes de hacérnosla llegar, con la finalidad de evitar cualquier contratiempo y disgusto en el futuro, recuerda que sobre aviso no hay engaño. </p>
-
-<p class="p1"><br>¿Qué pasa si no pude enviar el archivo de mi selección a tiempo?</p>
-
-<p class="p2"><br>El no enviarnos tu archivo con la lista de tu selección de fotos lo antes posible,  implica que tu paquete tenga que ser colocado en lista de espera y quede rezagado por debajo de otros proyectos que si hayan sido recibidos correctamente en tiempo y orden, en este caso tendrás que esperar tu turno para poder continuar con la siguiente fase del proceso, te recordamos que este tiempo puede aumentar considerablemente de acuerdo a la cantidad de trabajos por encima de tu paquete, lo que se reflejara también en un retraso considerable en los tiempos de entrega. Por lo tanto entre más pronto nos envíes tu lista de selección de forma correcta, mas rápido podremos iniciar con el resto del proceso y entregarte así tu paquete terminado.  </p>
-
-<p class="p1"><br>¿Cuando el link de mi galería deja de funcionar se borran todas mis fotos?  </p>
-
-<p class="p2"><br>Por supuesto que no, solamente la galería virtual con las fotos de muestra en baja resolución quedara inactiva, pero tus fotos originales en alta resolución seguirán respaldadas en nuestras bases de datos, hasta por un periodo máximo de 6 meses posteriores a la fecha de tu boda, así que tranquilo todo mundo.</p>
-
-<p class="p1"><br>¿Qué pasa si al recibir mi paquete tiene algún daño, falta alguna pieza o no funciona correctamente? </p>
-
-<p class="p2"><br>Nuestro compromiso contigo al contratar nuestros servicios es proporcionarte un producto de la más alta calidad, en este aspecto todos y cada uno de nuestros paquetes están garantizados por defectos de fabrica, por lo que si al recibir el tuyo detectas que alguna de las inclusiones en el mismo no funciona correctamente, está dañada o contiene errores de impresión, solo necesitas enviarnos un correo electrónico notificándonos el problema y especificando cual es la parte del paquete que está dañada. Nosotros iniciaremos una pequeña investigación, donde te pediremos alguna información adicional y quizás tengamos que solicitarte que nos devuelvas la pieza dañada para corroborar el fallo, si el error es nuestro te repondremos la parte de tu paquete dañada sin ningún costo extra. Sin embargo en caso de que el daño fuese provocado como consecuencia de un mal manejo o por perdida o robo total o parcial del paquete por parte del personal del servicio de paquetería, la empresa no se hará responsable por ningún motivo y cualquier reposición y costo de reenvío, tendrás que arreglarlo personalmente con la paquetería.</p>
->>>>>>> 9d34b7ca2a7223ed31296fd7d4efd29dc03d1a9a
-
-<p class="p2"><br>Si todavía tienes alguna duda o deseas consultar algún otro detalle puedes hacerlo directamente a través del chat, el cual ponemos a tu disposición en nuestra “sección de contacto,” en donde con gusto nuestro personal te atenderá en tiempo real. Solo debes consultar el status del mismo para saber si alguien de nuestro personal está disponible en ese momento o si lo prefieres, también puedes contactarnos directamente por teléfono poner teléfono de contacto aquí, o bien a través de nuestro servicio de correo electrónico escribir el correo electrónico de información aquí donde de igual manera con gusto te atenderemos a la mayor brevedad posible. Recuerda que estamos a tus órdenes.</p>
-                                
-                                <!-- Termina -->
-                                
-                             </div>
-                        </div>
-                    </div>
+                    
                 </div>
             
             
